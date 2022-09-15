8a33e587984b802f282e29b6714244af8d1b836b
Orlandoj77 <laudesjones@gmail.com>
2022-09-15 18:54:24 +0100

    nothing

--- src/components/butthon/button_download.tsx
+++ src/components/butthon/button_download.tsx
@@ -1,25 +1,20 @@
 import React from "react";
 import {Container, Button} from "./style/style"
 
-
-class Button_footer extends React.Component {
-    render() {
-      return (
-        <div>
-          Olá, {this.props.name}!
-        </div>
-      );
-    }
-  }
-
-
 function Button_download (){
     return(
         <>
           <Container>
         <Button> <span>Download</span><i></i></Button>
-       </Container>
-      
+
+       { /*<div>
+        <button className="cssbuttons-io-button"> Get started
+       <div className="icon">
+       <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
+       </div>
+       </button>
+    </div>*/}
+        </Container>
         </>
       
     )
